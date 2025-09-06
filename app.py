071984d6dc5d50aeb9dbe2b647d70bb898be9629
Camay01 <75464408+Camay01@users.noreply.github.com>
2025-09-06 10:13:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel  #validates the req automatically
 import os
-import httpx
+import httpx                    #server can handle multipe req at a time
 
 app = FastAPI()
 
 class Statement(BaseModel):
     statement: str
-
+    
+#environmental variables
 @app.post("/validate")
 async def validate(statement: Statement):
     provider = os.getenv("PROVIDER", "none")
     api_key = os.getenv("OPENAI_API_KEY", None)
     model = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
-
+    
+#when OPENAI is configured-
     if provider == "openai" and api_key:
         async with httpx.AsyncClient() as client:
             resp = await client.post(
